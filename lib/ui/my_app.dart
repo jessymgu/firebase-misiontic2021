a2f47adcaa2b25bc0e243ae289ca897f60b298d3
Jessy Moreno <katemgu@gmail.com>
2021-12-16 19:21:47 -0500

    Add: first commit

---
+++ lib/ui/my_app.dart
@@ -0,0 +1,58 @@
+import 'package:f_202110_firebase/domain/controller/authentication_controller.dart';
+import 'package:f_202110_firebase/domain/controller/chat_controller.dart';
+import 'package:f_202110_firebase/domain/controller/firestore_controller.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'firebase_central.dart';
+
+class MyApp extends StatelessWidget {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Firebase demo - MisionTIC',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+          body: FutureBuilder(
+        future: _initialization,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            print("error ${snapshot.error}");
+            return Wrong();
+          }
+          if (snapshot.connectionState == ConnectionState.done) {
+            Get.put(FirebaseController());
+            Get.put(AuthenticationController());
+            Get.put(ChatController());
+            return FirebaseCentral();
+          }
+
+          return Loading();
+        },
+      )),
+    );
+  }
+}
+
+class Wrong extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(child: Text("Something went wrong")),
+    );
+  }
+}
+
+class Loading extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(child: Text("Loading")),
+    );
+  }
+}
